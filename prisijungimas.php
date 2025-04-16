a73474f3999c83c39700a8ce5762b535c8872a47
Amandagra30 <amandagrakalskiene@gmail.com>
2025-04-16 21:27:40 +0300

    bandymas sukurti žinučių išsiuntimo formą

--- prisijungimas.php
+++ prisijungimas.php
@@ -2,12 +2,38 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "Test";
-//duomenų bazės pavadinimas
+$dbname = "mano_projektas"; // ← Pakeisk į savo tikrą DB pavadinimą
 
-$conn = new mysqli($servername,$username,$password,$dbname);
-if ($conn->connect_error) {
-    die("Nepavyko prisijungti prie duomenų bazės:" .$conn->connect_error);
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Tikrinam prisijungimą
+// if ($conn->connect_error) {
+//     die("Prisijungimo klaida: " . $conn->connect_error);
+// }
+
+// Gaunam duomenis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $vardas = $_POST['vardas'] ?? '';
+    $el_pastas = $_POST['el_pastas'] ?? '';
+    $zinute = $_POST['zinute'] ?? '';
+
+    if ($vardas && $el_pastas && $zinute) {
+        $sql = "INSERT INTO klientai(vardas, el_pastas, zinute) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        if ($stmt) {
+            $stmt->bind_param("sss", $vardas, $el_pastas, $zinute);
+            $stmt->execute();
+            echo "Duomenys sėkmingai išsaugoti!";
+            $stmt->close();
+        } else {
+            echo "Klaida ruošiant SQL: " . $conn->error;
+        }
+    } else {
+        echo "Visi laukai yra privalomi.";
+    }
+} else {
+    echo "Duomenys turi būti siunčiami POST metodu.";
 }
-echo "Prisijungimas sėkmingas!";
+
+$conn->close();
 ?>
